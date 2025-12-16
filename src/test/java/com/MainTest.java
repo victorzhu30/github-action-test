4a881349dfcbbc763795a84d1b03b9ed16beddcd
Victor Zhu <2364305645@qq.com>
2025-12-16 19:42:36 +0800

    Revise test case

--- src/test/java/com/MainTest.java
+++ src/test/java/com/MainTest.java
@@ -7,10 +7,35 @@ import org.junit.jupiter.api.Test;
 class MainTest {
 
 	@Test
-	void test() {
+	void addsTwoPositiveNumbers() {
 		Main main = new Main();
 		int result = main.add(1, 1);
-		assertEquals(1, result);
+		assertEquals(2, result);
+	}
+
+	@Test
+	void throwsIllegalArgumentWhenANegative() {
+		Main main = new Main();
+		assertThrows(IllegalArgumentException.class, () -> main.add(-1, 5));
+	}
+
+	@Test
+	void throwsIllegalArgumentWhenBNegative() {
+		Main main = new Main();
+		assertThrows(IllegalArgumentException.class, () -> main.add(5, -1));
+	}
+
+	@Test
+	void throwsArithmeticWhenOverflow() {
+		Main main = new Main();
+		assertThrows(ArithmeticException.class, () -> main.add(Integer.MAX_VALUE, 1));
+	}
+
+	@Test
+	void addsAtOverflowBoundary() {
+		Main main = new Main();
+		int result = main.add(Integer.MAX_VALUE - 1, 1);
+		assertEquals(Integer.MAX_VALUE, result);
 	}
 
 }
